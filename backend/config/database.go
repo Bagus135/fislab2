6f077a71a1add4eeb3cd40ef6774a916bfe6af3c
sweetnebulae <magellaniccloud21@gmail.com>
2025-02-03 17:23:11 +0700

    fix db connect

--- backend/config/database.go
+++ backend/config/database.go
@@ -1,9 +1,22 @@
-package config
+package database
 
 import (
 	"backend/prisma/db"
+	"log"
 )
 
-type DB struct {
-	Prisma *db.PrismaClient
+// ConnectDB menginisialisasi dan mengembalikan client Prisma yang terhubung ke database.
+func ConnectDB() *db.PrismaClient {
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	return client
+}
+
+// DisconnectDB from database
+func DisconnectDB(client *db.PrismaClient) {
+	if err := client.Prisma.Disconnect(); err != nil {
+		log.Printf("Failed to disconnect from database: %v", err)
+	}
 }
--- backend/main.go
+++ backend/main.go
@@ -1,37 +1,29 @@
 package main
 
 import (
+	"backend/config"
 	"backend/handler"
-	"backend/prisma/db"
 	"backend/router"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// Buat instance Prisma Client
-	client := db.NewClient()
-	if err := client.Prisma.Connect(); err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer func() {
-		if err := client.Prisma.Disconnect(); err != nil {
-			log.Printf("Failed to disconnect from database: %v", err)
-		}
-	}()
+	// DB connection
+	client := database.ConnectDB()
+	defer database.DisconnectDB(client)
 
-	// Buat instance AuthHandler
+	// Instance handler
 	authHandler := handler.NewAuthHandler(client)
 	announcementHandler := handler.NewAnnouncementHandler(client)
 	userHandler := handler.NewUserProfile(client)
 
-	// Initialising router
+	// Initializing router
 	r := router.NewRouter(authHandler, announcementHandler, userHandler)
 
-	// Jalankan server
+	// Running server
 	log.Println("Server started on :8080")
-	if err := http.ListenAndServe(
-		":8080", r); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
